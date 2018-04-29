9fb85cfcdd80726a1402b30304a691b216003e48
Gabriel Stedile <gabriel.stedile9@gmail.com>
2018-04-28 23:56:13 -0300

    Search and video list with React

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 React Youtube app training
 
+used dependencies:
+
+youtube-api-search
+lodash (controls when a function will be called)
 
 
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
+const API_KEY = 'AIzaSyADgjB5b-QGyusS4_I9rlfUvBurEPA1p7g';
+
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        videos: [],
+        selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+          this.setState({
+              videos,
+              selectedVideo: videos[0]
+          });
+      });
+  }
+
   render() {
+    const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
+
     return (
-      <div>React simple starter</div>
+        <div>
+         <SearchBar onSearchTermChange={videoSearch}/>
+         <VideoDetail video={this.state.selectedVideo}/>
+         <VideoList
+             onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+             videos={this.state.videos}/>
+        </div>
     );
   }
 }
